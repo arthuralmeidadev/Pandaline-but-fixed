5be4a3d2ccc00bed751a80c828bb0bdc8a060616
Arthur Almeida <arthuralmeida.office.dev@gmail.com>
2023-06-13 00:20:03 -0300

    /feat local route controller function

--- src/routers/protected.router.js
+++ src/routers/protected.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import protectedController from "../controllers/protected.controller.js";
+import publicController from "../controllers/public.controller.js";
 import authenticationMiddleware from "../middlewares/authentication.middleware.js";
 
 const router = Router();
 
 router.use(authenticationMiddleware);
 
-router.get("/logout", protectedController.logout);
+router.get("/", protectedController.dashboard);
+router.get("/logout", publicController.logout);
 
 export default router;
