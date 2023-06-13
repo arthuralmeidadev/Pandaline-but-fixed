5bdb4b1e6e2fef113b1a4f770977b726099dbbe0
Arthur Almeida <arthuralmeida.office.dev@gmail.com>
2023-06-13 00:18:29 -0300

    /ref user ctrl moved to public controller

--- src/controllers/public.controller.js
+++ src/controllers/public.controller.js
@@ -1,4 +1,7 @@
 import fs from "fs-extra";
+import { authentication } from "../helpers/authentication.js";
+import { tokenCookieOptions } from "../config/tokenCookie.config.js";
+import errors from "../config/errors.config.js";
 
 async function homepage(req, res, next) {
   try {
@@ -24,7 +27,59 @@ async function viewProduct(req, res, next) {
   };
 };
 
+async function login(req, res, next) {
+  const { id, secret } = req.body;
+  try {
+    await authentication.verifyUserCredentials(id, secret);
+    const accessTokenPayload = await authentication.encrypt({ id, secret });
+    const refreshTokenPayload = await authentication.encrypt({ id });
+    const { accessToken, refreshToken } = await authentication.grabTokens(accessTokenPayload, refreshTokenPayload);
+
+    res.cookie("accessToken", accessToken, tokenCookieOptions);
+    res.cookie("refreshToken", refreshToken, tokenCookieOptions);
+
+    return res.redirect("/admin");
+
+  } catch (error) {
+    next(error);
+  };
+};
+
+async function logout(req, res, next) {
+  try {
+    res.clearCookie("accessToken", tokenCookieOptions);
+    res.clearCookie("refreshToken", tokenCookieOptions);
+
+    return res.redirect("/");
+
+  } catch (error) {
+    next(error);
+  };
+};
+
+async function refreshAccess(req, res, next) {
+  const { refreshToken } = req?.cookies;
+  try {
+    if (!refreshToken)
+      throw errors.forbidden;
+
+    const refreshTokenPayload = await authentication.verifyToken(refreshToken);
+    const decryptedData = await authentication.decrypt(refreshTokenPayload);
+    const accessToken = await authentication.getNewAccessToken(decryptedData);
+
+    res.cookie("accessToken", accessToken, tokenCookieOptions);
+
+    return res.redirect(req.query.stayin);
+
+  } catch (error) {
+    next(error);
+  };
+};
+
 export default {
   homepage,
-  viewProduct
+  viewProduct,
+  login,
+  logout,
+  refreshAccess
 };
--- src/routers/public.router.js
+++ src/routers/public.router.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import publicController from "../controllers/public.controller.js";
-import protectedController from "../controllers/protected.controller.js";
 
 const router = Router();
 
 router.get("/", publicController.homepage);
 router.get("/view-product", publicController.viewProduct);
-router.post("/login", protectedController.login);
-router.get("/refresh-access", protectedController.refreshAccess);
+router.post("/login", publicController.login);
+router.get("/refresh-access", publicController.refreshAccess);
 
 export default router;
